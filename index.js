1bb8189e3a29e425f993141db90286162d323f0c
Paul Jang <pjang91@gmail.com>
2023-10-13 08:01:07 -0400

    papaparse

---
+++ index.js
@@ -0,0 +1,11 @@
+const file_inp = document.getElementById("file_inp");
+const upload = document.getElementById("upload");
+
+upload.addEventListener("click", () => {
+  console.log(file_inp);
+});
+
+//papa parse function 6:30 YouTube Link
+//https://youtu.be/s2PKu_HvIHU?si=Y-mDhoNisefErZX5
+
+function PapaParse ()
